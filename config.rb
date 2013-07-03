f76308a2b813735302437bbfd4e73dfe7fcf5a74
yaronshai <yaron.shai@aol.com>
2013-07-03 06:28:24 +0300

    init

---
+++ config.rb
@@ -0,0 +1,18 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/boston/"
+http_images_path = "/boston/images"
+http_fonts_path = "/boston/fonts"
+css_dir = "public/boston/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
